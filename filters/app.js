33a084b37f69a0cdfc80656137810b12cdf1833c
masoudso <veisi.masoud@gmail.com>
2017-07-12 14:55:44 +0430

    creating filters

--- filters/app.js
+++ filters/app.js
@@ -1,15 +1,37 @@
 (function () {
   'use strict';
   angular.module('filtApp', [])
-  .controller('filtController', filtController);
-filtController.$inject = ['$scope', '$filter'];
+  .controller('filtController', filtController)
+  .filter('names', nameFilter);
+filtController.$inject = ['$scope', '$filter', 'namesFilter'];
 
-function filtController($scope, $filter){
+function filtController($scope, $filter, namesFilter){
+  $scope.cost = 240;
   $scope.message = function(){
   var message = "You are not different from still water!"
   var uppmsg = $filter('uppercase')(message);
   return uppmsg;
   }
+
+
+  $scope.saymessage = function (){
+    var msg = "one of the best aircrafts you can use for intercontinental routes is Airbus-a330";
+     return msg;
+  }
+
+  $scope.saySecMessage = function (){
+    var msg = "one of the best aircrafts you can use for intercontinental routes is Airbus-a330";
+     msg = namesFilter(msg);
+     return msg;
+  }
+};
+
+function nameFilter(){
+  return function (input){
+    input = input || "";
+    input = input.replace("Airbus-a330" , "Boeing777");
+    return input;
+  };
 };
 
 })();
